20bee75369f73cda8a518a1994da6d5c61d21bb4
SergeyESh <SergeyESh1989@yandex.ru>
2022-11-22 11:29:55 +0300

    Add headless

--- src/test/java/ru/netology/OrderCardInvalidValueTest.java
+++ src/test/java/ru/netology/OrderCardInvalidValueTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,7 +16,11 @@ public class OrderCardInvalidValueTest {
 
     @BeforeEach
     void createBrowser() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @Test
